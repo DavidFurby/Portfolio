6db12ecb90365a10de303844eba0bfda87b28bb6
David Furby <d55d@live.se>
2021-06-23 11:02:44 +0200

    Updated main-page

--- pages/experience.js
+++ pages/experience.js
@@ -1,4 +1,4 @@
-import { Container, Col, Row, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import styles from '../styles/experience.module.css'
 
 const experience = () => {
@@ -8,24 +8,21 @@ const experience = () => {
                 <div className={styles.imgContainer}>
                     <div className={styles.imgShadow}>
                         <div className="container text-center">
-                            <Image alt="krogisLogo" src="/Krogis.png" fluid />
-                            <Image alt="krogisExample" className="w-50 mt-5" src="/KrogisExample.png" fluid />
+                            <Image className={styles.img} alt="krogisExample"  src="/KrogisExample.png" fluid />
                         </div>
                     </div>
                 </div>
                 <div className={styles.imgContainer}>
                     <div className={styles.imgShadow}>
                         <div className="container text-center">
-                            <Image alt="svenskParfymLogo" src="/Krogis.png" fluid />
-                            <Image alt="svenskParfymExample" className="w-50 mt-5" src="/SvenskParfymExample.png" fluid />
+                            <Image className={styles.img} alt="svenskParfymExample" src="/SvenskParfymExample.png" fluid />
                         </div>
                     </div>
                 </div>
                 <div className={styles.imgContainer}>
                     <div className={styles.imgShadow}>
                         <div className="container text-center">
-                            <Image alt="yhKollenLogo" src="/Krogis.png" fluid />
-                            <Image alt="yhKollenExample" className="w-50 mt-5" src="/YH-KollenExample.png" fluid />
+                            <Image className={styles.img} alt="yhKollenExample" src="/YH-KollenExample.png" fluid />
                         </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <div className="container mt-5">
       <div className="row justify-content-between">
         <div className="col">
-          <h1 className="display-1">David Furby</h1>
-          <p>Jag heter David Furby och är 23 år. Nyligen gjorde jag klart min utbildning i Java och
+          <h1 className={`display-1 ${styles.title}`}>David Furby</h1>
+          <p className={`${styles.desc}`}>Jag heter David Furby och är 23 år. Nyligen gjorde jag klart min utbildning i Java och
             webbutvecklings på yrkeshögskolan i Mölndal, och söker just nu anställning. Jag har
             studerat på Campus Mölndal i två år, och har bland annat jobbat med Java, JavaScript,
             React, Vue, Native, PostgreSQL, Firebase, Spring Boot och MongoDB. Under studietiden har jag även jobbat med ett startupföretag som heter Krogis. Där jag har rollen att bygga deras
@@ -19,19 +19,19 @@ export default function Home() {
             <Image className={styles.img} src="/Me.jpg" fluid />
             <div className="container">
               <div className="row justify-content-around mt-5">
-                <div  className="col-md-auto" >
+                <div className="col-md-auto" >
                   <a href="https://github.com/DavidFurby">
-                    <Image alt="github" src="/Github.png" width={42} height={41.02} />
+                    <Image className={styles.icon} alt="github" src="/Github.png" width={42} height={41.02} />
                   </a>
                 </div>
                 <div className="col-md-auto">
                   <a href="https://www.linkedin.com/in/david-furby-62a40b134/">
-                    <Image alt="linkedIn" src="/LinkedIn.png" width={42} height={41.02} />
+                    <Image className={styles.icon} alt="linkedIn" src="/LinkedIn.png" width={42} height={41.02} />
                   </a>
                 </div>
               </div>
             </div>
-            </div>
+          </div>
 
         </div>
       </div>
